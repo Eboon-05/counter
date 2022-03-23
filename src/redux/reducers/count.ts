9f39f321c21fc021f11f3bbc9aefc265355e6368
Eban Sayago <eban.sayago@protonmail.com>
2022-03-23 19:05:56 -0300

    Improved count reducer

--- src/redux/reducers/count.ts
+++ src/redux/reducers/count.ts
@@ -1,27 +1,42 @@
 import { Reducer } from "redux"
 
-export interface CountState {
-    data: string[]
+export interface Count {
+    name: string,
+    value: number
 }
 
-interface Action {
-    type: 'ADD_COUNT',
-    payload: string
+export interface CountState {
+    readonly data: Count[],
+    readonly count?: Count
+}
+
+export interface CountAction {
+    type: 'ADD_COUNT' | 'SET_COUNTS',
+    payload: Count | Count[] | string,
 }
 
 const initialState = {
     data: []
 }
 
-export const countReducer: Reducer<CountState, Action> = (
+function isCount (data: any): data is Count {
+    return ('name' in data && 'value' in data)
+}
+
+export const countReducer: Reducer<CountState, CountAction> = (
     state = initialState,
     action
 ) => {
     switch (action.type) {
         case 'ADD_COUNT':
-            return {
-                ...state,
-                data: [...state.data, action.payload]
+            if (isCount(action.payload)) {
+                return {
+                    ...state,
+                    data: [...state.data, action.payload]
+                }
+            } else {
+                console.error(`action.payload is not a Count`)
+                return state
             }
         default:
             return state
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { CountState } from '../redux/reducers/count'
+import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
+import { CountAction, CountState } from '../redux/reducers/count'
 
 function Home() {
     const data = useSelector<CountState, CountState['data']>(state => state.data)
+    const dispatch: Dispatch<CountAction> = useDispatch()
 
-    console.log(data)    
+    // This should log an error
+    // dispatch({
+    //     type: 'ADD_COUNT',
+    //     payload: []
+    // })
 
     return (
         <div>Home</div>

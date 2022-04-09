00009330f1336b5418c1f4df9bba4e87878cc107
Eban Sayago <eban.sayago@protonmail.com>
2022-04-08 21:41:06 -0300

    :construction: Added background

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux'
 
 import { store } from './redux/store'
 
-// Styles
+// Styles & assets
 import './styles/main.css'
+import gradient from './assets/gradient-bg-1.svg'
 
 // Routes
 import { Home } from './routes/Home'
@@ -18,6 +19,7 @@ ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Header />
+			<img className='fixed min-w-[800px] min-h-[110vh] md:min-w-[180vw] -left-5 -inset-y-5 -z-10 blur-lg' src={gradient} />
 			<Router>
 				<Routes>
 					<Route path='/' element={<Home />} />

ce9c4e8bd574f7f545ed06d5f1685a386df12108
Eban Sayago <eban.sayago@protonmail.com>
2022-04-12 19:27:25 -0300

    :fire: Removed unusued var

--- src/routes/CreateNewCount.tsx
+++ src/routes/CreateNewCount.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Dispatch } from 'redux'
 import { useDispatch, useSelector } from 'react-redux'

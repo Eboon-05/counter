7d89089f7baf8e84c4fb25ac11e815136f2cbe04
Eban Sayago <eban.sayago@protonmail.com>
2022-04-12 18:55:48 -0300

    :lipstick: Improved Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEventHandler, ReactNode } from 'react'
 
 interface Props {
-    type: 'primary',
+    type: 'primary' | 'danger',
     children: ReactNode,
     onClick?: MouseEventHandler,
     className?: string,
@@ -17,9 +17,15 @@ function Button(props: Props) {
     } = props
 
     return (<button className={`
-        p-2
+        p-2 font-semibold
+        flex items-center justify-center
 
-        ${type === 'primary' && `bg-black rounded-lg shadow-md hover:shadow-lg 
+        ${type === 'primary' && `bg-black rounded-lg shadow-black shadow-sm 
+        hover:shadow-md hover:shadow-black 
+        transition-all duration-500 disabled:opacity-70 disabled:shadow-none`}
+
+        ${type === 'danger' && `bg-red-500 text-black shadow-red-500 rounded-lg shadow-sm
+        hover:shadow-md hover:shadow-red-500
         transition-all duration-500 disabled:opacity-70 disabled:shadow-none`}
         
         ${className}
